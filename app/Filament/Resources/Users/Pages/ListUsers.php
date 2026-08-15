47c355cc4773864170eb6de45df37385e1ce5940
Matthew Ballou <mballou@kirschbaumdevelopment.com>
2026-08-14 22:02:05 -0400

    Give the users list a create button
    With public registration closed, the backstage is the only way an account
    begins, and the list page had no way to reach the create page.

--- app/Filament/Resources/Users/Pages/ListUsers.php
+++ app/Filament/Resources/Users/Pages/ListUsers.php
@@ -5,9 +5,24 @@ declare(strict_types=1);
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
+
+    /**
+     * With public registration closed this button is the only way into the only
+     * page that can create an account.
+     *
+     * @return array<int, CreateAction>
+     */
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()
+                ->label('New account'),
+        ];
+    }
 }
